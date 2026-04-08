e3e2e169c75821968b40b48cceb53fcd7a421a33
M. Ilham Fathurrahman <ilham270305@gmail.com>
2026-04-08 20:30:42 +0700

    set model

--- lentera/app/Models/ValidasiVerifikasi.php
+++ lentera/app/Models/ValidasiVerifikasi.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class ValidasiVerifikasi extends Model
 {
-    //
+    protected $table = 'validasi_verifikasi';
+    protected $primaryKey = 'id_validasi';
+
+    protected $fillable = [
+        'id_pengajuan',
+        'id_admin',
+        'status_validasi',
+        'catatan',
+        'tanggal_verifikasi',
+    ];
+
+    public function pengajuan()
+    {
+        return $this->belongsTo(PengajuanBantuan::class, 'id_pengajuan');
+    }
+
+    public function admin()
+    {
+        return $this->belongsTo(User::class, 'id_admin');
+    }
 }
