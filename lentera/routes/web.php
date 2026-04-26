e02891436cadb596147323a133112c30c2558bf3
mfachrihafizhan <frieltzy8@gmail.com>
2026-04-26 19:45:01 +0700

    git commit -m "PBI-19 PBI #20 - Rekomendasi Penerima - Prioritas Penerima"

--- lentera/routes/web.php
+++ lentera/routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Recipient;
 
-Route::get('/', function () {
-    return view('welcome');
+// HITUNG ULANG SEMUA (sekali jalan)
+Route::get('/hitung', function () {
+    $data = Recipient::all();
+
+    foreach ($data as $d) {
+        $d->save(); // trigger auto scoring
+    }
+
+    return "OK";
 });
+
+// LIHAT RANKING
+Route::get('/ranking', function () {
+    $data = Recipient::orderByDesc('score')->get();
+
+    foreach ($data as $d) {
+        echo $d->name . " | Skor: " . $d->score . "<br>";
+    }
+});
+
+use App\Http\Controllers\RecommendationController;
+
+Route::get('/rekomendasi', [RecommendationController::class, 'index']);
+
+use App\Http\Controllers\RecipientController;
+
+Route::post('/recipient/store', [RecipientController::class, 'store']);
