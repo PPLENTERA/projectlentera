b9928053610c0421771db657b772a6e9f4c65ae8
mfachrihafizhan <frieltzy8@gmail.com>
2026-05-03 10:18:40 +0700

    git commit -m "PBI-25 PBI #24 - Landing Page"

--- lentera/routes/web.php
+++ lentera/routes/web.php
@@ -1,20 +1,43 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 use App\Models\Recipient;
+use App\Http\Controllers\RecommendationController;
+use App\Http\Controllers\RecipientController;
 
-// HITUNG ULANG SEMUA (sekali jalan)
+/*
+|--------------------------------------------------------------------------
+| Landing Page
+|--------------------------------------------------------------------------
+*/
+Route::get('/', function () {
+    return view('landing-page-lentera', [
+        'totalDana' => 12400000000000,
+        'totalPenerima' => Recipient::count(),
+    ]);
+});
+
+/*
+|--------------------------------------------------------------------------
+| Hitung Ulang Score
+|--------------------------------------------------------------------------
+*/
 Route::get('/hitung', function () {
     $data = Recipient::all();
 
     foreach ($data as $d) {
-        $d->save(); // trigger auto scoring
+        $d->save();
     }
 
     return "OK";
 });
 
-// LIHAT RANKING
+/*
+|--------------------------------------------------------------------------
+| Ranking
+|--------------------------------------------------------------------------
+*/
 Route::get('/ranking', function () {
     $data = Recipient::orderByDesc('score')->get();
 
@@ -23,10 +46,16 @@ Route::get('/ranking', function () {
     }
 });
 
-use App\Http\Controllers\RecommendationController;
-
+/*
+|--------------------------------------------------------------------------
+| Recommendation
+|--------------------------------------------------------------------------
+*/
 Route::get('/rekomendasi', [RecommendationController::class, 'index']);
 
-use App\Http\Controllers\RecipientController;
-
+/*
+|--------------------------------------------------------------------------
+| Store Recipient
+|--------------------------------------------------------------------------
+*/
 Route::post('/recipient/store', [RecipientController::class, 'store']);
